05b51d7b17421099dca7555f4b8d7e3ae32319b1
zhouyang <ashu0000008@163.com>
2019-08-26 18:20:04 +0800

    初始化首页

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 import 'net/api.dart';
 
 void main() => runApp(MyApp());
@@ -48,10 +49,9 @@ class _MyHomePageState extends State<MyHomePage> {
   String _counter = "0";
 
   void _incrementCounter() {
-
-    API.get_crypto_percent("btc").then((percent){
+    API.get_crypto_percent("btc").then((percent) {
       setState(() {
-        print("-----------------"+percent);
+        print("-----------------" + percent);
         var percentDouble = double.parse(percent) * 100;
         _counter = percentDouble.toStringAsFixed(2);
       });
@@ -68,6 +68,13 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _incrementCounter();
+  }
+
+  @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
     // by the _incrementCounter method above.
@@ -81,37 +88,19 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              '百分比'+'$_counter' + '%',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.refresh),
+      body: new ListView(
+        children: <Widget>[
+          new ListTile(
+            title: new Text('大饼占比 ' + '$_counter' + '%'),
+            onTap: _incrementCounter,
+          ),
+          new ListTile(
+            title: new Text('币种排序'),
+          ),
+          new ListTile(
+            title: new Text('平台排序'),
+          )
+        ],
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
